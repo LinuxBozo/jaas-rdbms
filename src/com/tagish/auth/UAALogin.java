41813ade9cbf6a7057a4dd121279a08a7a1ac90f
M. Adam Kendall <michael.kendall@gsa.gov>
2016-08-23 16:34:48 -0400

    Set user from uaa origin to idp origin on successful login if required

--- src/com/tagish/auth/UAALogin.java
+++ src/com/tagish/auth/UAALogin.java
@@ -15,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
  * Simple database based authentication module.
  *
  */
-public class BCryptDBLogin extends SimpleLogin
+public class UAALogin extends SimpleLogin
 {
 	protected String                dbDriver;
 	protected String                dbURL;
@@ -24,9 +24,11 @@ public class BCryptDBLogin extends SimpleLogin
 	protected String                userTable;
 	protected String                userColumn;
 	protected String                passColumn;
+	protected String 				originColumn;
+	protected String 				idpOrigin;
 	protected String                where;
 
-	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(4);
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
 	protected synchronized Vector validateUser(String username, char password[]) throws LoginException
 	{
@@ -41,28 +43,32 @@ public class BCryptDBLogin extends SimpleLogin
 			   con = DriverManager.getConnection(dbURL, dbUser, dbPassword);
 			else
 			   con = DriverManager.getConnection(dbURL);
-
-			psu = con.prepareStatement("SELECT " + passColumn + " FROM " + userTable +
+			psu = con.prepareStatement("SELECT " + passColumn + ", " + originColumn + " FROM " + userTable +
 									   " WHERE " + userColumn + "=?" + where);
 
 			/* Set the username to the statement */
 			psu.setString(1, username);
 			rsu = psu.executeQuery();
 			if (!rsu.next()) throw new FailedLoginException("Unknown user");
-			String db_pwd = rsu.getString(1);
+			String db_pwd = rsu.getString("password");
+			String db_origin = rsu.getString(originColumn).trim();
 			String in_pwd = new String(password);
-
-			String in_pwd_enc = passwordEncoder.encode(in_pwd);
-			String in_pwd_enc2 = passwordEncoder.encode(in_pwd);
+			System.err.println("Origin:   " + db_origin);
 
 			/* Check the password */
-			System.err.println("Database:  " + db_pwd);
-			System.err.println("Entered:   " + in_pwd);
-			System.err.println("Encrypted: " + in_pwd_enc);
-			System.err.println("Matches DB:" + passwordEncoder.matches(in_pwd, db_pwd));
-			System.err.println("Matches IN:" + passwordEncoder.matches(in_pwd, in_pwd_enc2));
-
 			if (!passwordEncoder.matches(in_pwd, db_pwd)) throw new FailedLoginException("Bad password");
+			psu.close();
+
+			if (db_origin.equalsIgnoreCase("uaa")) {
+				System.err.println("UAA user!");
+				psu = con.prepareStatement("UPDATE " + userTable + " SET " + originColumn + "=?" +
+											" WHERE " + userColumn + "=?");
+				psu.setString(1, idpOrigin);
+				psu.setString(2, username);
+				int rowsUpdated = psu.executeUpdate();
+				System.err.println("Updated " + rowsUpdated + " user(s)");
+				psu.close();
+			}
 
 			Vector p = new Vector();
 			p.add(new TypedPrincipal(username, TypedPrincipal.USER));
@@ -100,9 +106,12 @@ public class BCryptDBLogin extends SimpleLogin
 		if ((dbUser == null && dbPassword != null) || (dbUser != null && dbPassword == null))
 		   throw new Error("Either provide dbUser and dbPassword or encode both in dbURL");
 
-		userTable    = getOption("userTable",    "User");
-		userColumn   = getOption("userColumn", "user_name");
-		passColumn   = getOption("passColumn",    "user_passwd");
+		userTable    = getOption("userTable",    "users");
+		userColumn   = getOption("userColumn", "username");
+		passColumn   = getOption("passColumn",    "password");
+		originColumn = getOption("originColumn",	"origin");
+		idpOrigin	 = getOption("idpOrigin", "uaa");
+
 		where        = getOption("where",        "");
 		if (null != where && where.length() > 0)
 			where = " AND " + where;
